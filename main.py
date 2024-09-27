6e85596537eafe146982925c6af1e4b1355899d2
SergTsch87 <sergtsch87@gmail.com>
2024-09-28 00:41:33 +0300

    Додав можливiсть змiйцi зростати за рахунок їжi + деякi змiни в кодi у main.py

--- main.py
+++ main.py
@@ -3,17 +3,33 @@ import pygame
 import time
 import random
 
+# to-do list:
+    # 1) Додати сітку для поля
+    # 2) Намалювати рамку для поля
+    # 3) Відобразити швидкість змійки на екран
+    # 4) Коли змійці задаємо напрям, протилежний до її поточного руху, - тоді:
+    #   а) Якщо довжина змійки = 1:
+    #        змійка просто зупиняється.
+    #   б) Якщо довжина змійки > 1:
+    #        гра завершується (так, наче змійка "наїхала" на саму себе)
+        # Але ж, так не повинно бути!..
+        # Змійка просто повинна продовжувати рухатись у своєму поточному напрямку
 
 def main():
     pygame.init()
 
     white = (255, 255, 255)
     black = (0, 0, 0)
-    red = (255, 0, 0)
-    blue = (0, 0, 255)
+    red = (213, 50, 80)
+    blue = (50, 153, 213)
+    yellow = (255, 255, 102)
+    green = (0, 255, 0)
 
-    dis_width = 800
-    dis_height = 600
+    # dis_width = 800
+    # dis_height = 600
+    # Для прототипу:
+    dis_width = 300
+    dis_height = 300
 
     dis = pygame.display.set_mode((dis_width, dis_height))
     pygame.display.set_caption("Змійка")
@@ -21,13 +37,18 @@ def main():
     clock = pygame.time.Clock()
 
     snake_block = 10
-    snake_speed = 30
+    snake_speed = 5
 
-    font_style = pygame.font.SysFont(None, 50)    
+    font_style = pygame.font.SysFont("bahnshrift", 25)
+    score_font = pygame.font.SysFont("comicsansms", 35)
+
+    def our_snake(snake_block, snake_list):
+        for x in snake_list:
+            pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
     
     def message(msg, color):
         mesg = font_style.render(msg, True, color)
-        dis.blit(mesg, [dis_width/3, dis_height/3])
+        dis.blit(mesg, [dis_width/6, dis_height/3])
 
     def game_loop():
         game_over = False
@@ -39,12 +60,15 @@ def main():
         x1_change = 0
         y1_change = 0
 
+        snake_list = []
+        length_of_snake = 1
+
         foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
         foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
 
         while not game_over:
             while game_close == True:
-                dis.fill(white)
+                dis.fill(blue)
                 message("You Lost! Press Q-Quit or C-Play Again", red)
                 pygame.display.update()
 
@@ -76,24 +100,40 @@ def main():
                         y1_change += snake_block
 
             if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-                game_over = True
+                game_close = True
+                # game_over = True
             
             x1 += x1_change
             y1 += y1_change
-            dis.fill(white)
-            pygame.draw.rect(dis, blue, [foodx, foody, snake_block, snake_block])
-            pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
+            dis.fill(blue)
+            pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+            # pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
+            snake_head = []
+            snake_head.append(x1)
+            snake_head.append(y1)
+            snake_list.append(snake_head)
+
+            if len(snake_list) > length_of_snake:
+                del snake_list[0]
+
+            for x in snake_list[:-1]:
+                if x == snake_head:
+                    game_close = True
+
+            our_snake(snake_block, snake_list)
 
             pygame.display.update()
 
             if x1 == foodx and y1 == foody:
-                print("Смакота!")
+                foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+                foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+                length_of_snake += 1
 
             clock.tick(snake_speed)
 
-        message("You lost", red)
-        pygame.display.update()
-        time.sleep(2)
+        # message("You lost", red)
+        # pygame.display.update()
+        # time.sleep(2)
 
         pygame.quit()
         quit()
