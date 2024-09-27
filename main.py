7fa9cbd11b9afc3444e4ad0eb9b989c4333b450f
SergTsch87 <sergtsch87@gmail.com>
2024-09-27 18:34:29 +0300

    Додав код iнiцiалiзацiї до main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #!usr/bin/env
+import pygame
 
 
 def main():
-    pass
+    pygame.init()
+    dis = pygame.display.set_mode((400, 300))
+    pygame.display.update()
+    pygame.quit()
+    quit()
+
 
 if __name__ == "__main__":
     main()
