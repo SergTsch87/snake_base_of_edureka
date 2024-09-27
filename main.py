ab1d3012a4c563dfbcf0143cfcfaa86ab6dcadc1
SergTsch87 <sergtsch87@gmail.com>
2024-09-27 18:45:49 +0300

    Додав цикл подiй while not game_over до main.py

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ def main():
     pygame.init()
     dis = pygame.display.set_mode((400, 300))
     pygame.display.update()
+    pygame.display.set_caption("Змійка")
+    game_over = False
+
+    while not game_over:
+        for event in pygame.event.get():
+            print(event)
+
     pygame.quit()
     quit()
 
