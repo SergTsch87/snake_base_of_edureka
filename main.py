c4a920880ea6dfb7bbc92c916423ec28a223f9c6
SergTsch87 <sergtsch87@gmail.com>
2024-09-27 18:58:49 +0300

    Створив Змiйку у main.py

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import pygame
 def main():
     pygame.init()
     dis = pygame.display.set_mode((400, 300))
-    pygame.display.update()
     pygame.display.set_caption("Змійка")
-    game_over = False
 
+    blue = (0, 0, 255)
+    red = (255, 0, 0)
+    
+    game_over = False
     while not game_over:
         for event in pygame.event.get():
-            print(event)
+            if event.type == pygame.QUIT:
+                game_over = True
+        
+        pygame.draw.rect(dis, blue, [200, 150, 10, 10])
+        pygame.display.update()
 
     pygame.quit()
     quit()
